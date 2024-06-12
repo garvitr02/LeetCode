6f9633e0297a744a91cfa6cd78e7f247bac509cc
Garvit Rastogi <111786577+garvitr02@users.noreply.github.com>
2024-06-12 14:44:19 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (34.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int low = 0;
+        int mid = 0;
+        
+        int n = nums.size();
+        
+        int high = n-1;
+        
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low ++;
+                mid ++;
+            }
+            
+            else if(nums[mid] == 1){
+                mid ++;
+            }
+            
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
     }
 };
