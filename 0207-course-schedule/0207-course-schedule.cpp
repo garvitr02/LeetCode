6298a9ea1323da796d31f7437332b479244adda0
Garvit Rastogi <111786577+garvitr02@users.noreply.github.com>
2024-08-06 16:23:05 +0530

    Time: 16 ms (57.08%), Space: 16.9 MB (79.21%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& prerequisites) {
+        
+        
+        vector<int> adj[V];
+        
+        for(int i=0; i<prerequisites.size(); i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        
+        vector<int> indegree(V);
+	    
+	    for (int i=0; i<V; i++){
+	        for(auto it : adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    
+	    for(int i=0; i<V; i++){
+	        if(indegree[i] == 0){
+	            q.push(i);
+	        }
+	    }
+	    
+	    vector<int> topo;
+	    
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        topo.push_back(node);
+	        
+	        for(auto it : adj[node]){
+	            indegree[it]--;
+	            if(indegree[it] == 0) q.push(it);
+	        }
+	    }
+        
+        if(topo.size() == V) return true;
+        return false;
+    }
+};
