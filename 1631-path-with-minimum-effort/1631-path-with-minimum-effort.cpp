978ec3ceca1d465ce6e7f93426759eeb28627553
Garvit Rastogi <111786577+garvitr02@users.noreply.github.com>
2024-08-08 15:22:35 +0530

    Time: 84 ms (75.10%), Space: 23.9 MB (53.25%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -23,7 +23,57 @@ public:
             int y = it.second.second;
             pq.pop();
             
-            if (x == n-1 && y == m-1) {
+            if (x == n-1 && y == m-1) {class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> effort(n, vector<int>(m, INT_MAX));
+        
+        effort[0][0] = 0;
+        
+        pq.push({0, {0,0}});
+        
+        int r[] = {-1, 0, +1, 0};
+        int c[] = {0, +1, 0, -1};
+        
+        
+        while(!pq.empty()){
+            auto it = pq.top();
+            int d = pq.top().first;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            pq.pop();
+            
+            if(x == n-1 && y == m-1){
+                return d;
+            }
+            
+            for(int i=0; i<4; i++){
+                
+                    int row = x + r[i];
+                    int col = y + c[i];
+                    if(row<n && row>=0 && col<m && col>= 0){
+                        int newEffort = max(d, abs(grid[row][col] - grid[x][y]));
+                            if (newEffort < effort[row][col]) {
+                                effort[row][col] = newEffort;
+                                pq.push({effort[row][col], {row, col}});
+                            }
+                    }
+                
+            }
+        }
+        
+        return effort[n-1][n-1];
+        
+        
+        
+        
+    }
+};
                 return d;
             }
             
