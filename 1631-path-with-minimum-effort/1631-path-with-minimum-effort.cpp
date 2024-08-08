24def744bda314bd459ed67fbbc5ab095f7e7175
Garvit Rastogi <111786577+garvitr02@users.noreply.github.com>
2024-08-08 15:27:39 +0530

    Time: 84 ms (75.10%), Space: 23.9 MB (53.25%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,30 +1,5 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        
-        int n = heights.size();
-        int m = heights[0].size();
-        
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        
-        vector<vector<int>> effort(n, vector<int>(m, INT_MAX));
-        
-        effort[0][0] = 0;
-        
-        pq.push({0, {0, 0}});
-        
-        int r[] = {-1, 0, +1, 0};
-        int c[] = {0, +1, 0, -1};
-        
-        while (!pq.empty()) {
-            auto it = pq.top();
-            int d = it.first;
-            int x = it.second.first;
-            int y = it.second.second;
-            pq.pop();
-            
-            if (x == n-1 && y == m-1) {class Solution {
-public:
     int minimumEffortPath(vector<vector<int>>& grid) {
         
         priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
@@ -74,22 +49,4 @@ public:
         
     }
 };
-                return d;
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                int row = x + r[i];
-                int col = y + c[i];
-                if (row >= 0 && row < n && col >= 0 && col < m) {
-                    int newEffort = max(d, abs(heights[row][col] - heights[x][y]));
-                    if (newEffort < effort[row][col]) {
-                        effort[row][col] = newEffort;
-                        pq.push({newEffort, {row, col}});
-                    }
-                }
-            }
-        }
-        
-        return effort[n-1][n-1];
-    }
-};
+
