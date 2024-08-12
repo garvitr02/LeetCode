9bed52e1c880a1124d9ce3ff571d38a085b59c49
Garvit Rastogi <111786577+garvitr02@users.noreply.github.com>
2024-08-12 16:35:13 +0530

    Time: 5 ms (20.22%), Space: 8.4 MB (25.22%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int r = 0;
+
+        while (x != 0) {
+            int rem = x % 10;
+            x /= 10;
+            
+            if (r > INT_MAX / 10 || r < INT_MIN / 10) return 0;
+            
+            r = r * 10 + rem;
+        }
+
+        return r;
+    }
+};
